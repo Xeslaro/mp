317b43aaa3b1010995e4992e702e476288d38a75
Xeslaro <phoooyb@gmail.com>
2013-04-04 19:03:46 +0000

    add argument for howmany nanoseconds a packet should be sent in impersonate

--- impersonate.c
+++ impersonate.c
@@ -21,7 +21,8 @@ int main(int c, char *z[])
 	char	msg[1526];
 	int	len = mk_arp_reply_packet(msg, "\xff\xff\xff\xff\xff\xff", victim, 0x0, conv_ip(z[2]), 1);
 	struct timespec	t;
-	t.tv_sec = 0, t.tv_nsec = 10000000;
+	sscanf(z[3], "%d", &t.tv_nsec);
+	t.tv_sec = 0;
 	while (1) {
 		errn1(sendto(packet_socket, msg, len, 0, (struct sockaddr*)&sa_ll, sizeof(struct sockaddr_ll)), "sendto error");
 		nanosleep(&t, NULL);
